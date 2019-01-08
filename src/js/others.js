d98be213594084fc0240600e8dec32f8b1e6017a
mekki <644435810@qq.com>
2019-01-08 10:52:13 +0800

    2

--- src/js/others.js
+++ src/js/others.js
@@ -84,12 +84,8 @@ export default {
   requestFullscreen() {
     const document = this.element.ownerDocument;
 
-    if (this.fulled && !(
-      document.fullscreenElement
-      || document.mozFullScreenElement
-      || document.webkitFullscreenElement
-      || document.msFullscreenElement
-    )) {
+    if (this.fulled && !document.fullscreenElement && !document.mozFullScreenElement
+      && !document.webkitFullscreenElement && !document.msFullscreenElement) {
       const { documentElement } = document;
 
       if (documentElement.requestFullscreen) {
@@ -105,12 +101,7 @@ export default {
   },
 
   exitFullscreen() {
-    if (this.fulled && (
-      document.fullscreenElement
-      || document.mozFullScreenElement
-      || document.webkitFullscreenElement
-      || document.msFullscreenElement
-    )) {
+    if (this.fulled) {
       const document = this.element.ownerDocument;
 
       if (document.exitFullscreen) {
