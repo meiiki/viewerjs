b85cf9e4976ed286059f37e358983216c44306a5
mekki <644435810@qq.com>
2019-01-10 15:36:37 +0800

    2

--- dist/viewer.js
+++ dist/viewer.js
@@ -1811,7 +1811,7 @@
       var onViewed = function onViewed() {
         var imageData = _this.imageData;
         var render = Array.isArray(options.title) ? options.title[1] : options.title;
-        title.innerHTML = isFunction(render) ? render.call(_this, image, imageData) : "".concat(alt, " (").concat(imageData.naturalWidth, " \xD7 ").concat(imageData.naturalHeight, ")");
+        title.innerHTML = isFunction(render) ? render.call(_this, image, imageData) : "".concat(alt, "");
       };
 
       var onLoad;
